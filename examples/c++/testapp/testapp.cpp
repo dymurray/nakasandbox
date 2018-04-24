6d3f501c6ef78fa2d2751da8dcdf6002f163e001
Dylan Murray <dymurray@redhat.com>
2018-04-24 16:38:19 -0400

    testapp

--- examples/c++/testapp/testapp.cpp
+++ examples/c++/testapp/testapp.cpp
@@ -17,11 +17,16 @@ int main()
 
     // Get an arbitrary message for signing
     // (The contents are unimportant for this example)
-    std::vector<uint8_t> message(16);
+    std::string msg = "dylan terrence nakamoto";
 
+    std::vector<uint8_t> message(msg.begin(), msg.end());
+    message.push_back('\0');
     // Get public and private keys
     KeyPtr key { std::make_unique<KeyECSecp256k1<>>() };
     PubKeyPtr pubKey { key->getPubKey() };
+
+    KeyPtr newKey = key->derive(message);
+    PubKeyPtr newPubKey = pubKey->derive(message);
 
     // Sign the message using our private key
     std::vector<uint8_t> signature { key->sign(message) };
@@ -29,7 +34,32 @@ int main()
     // Verify the signed message using the corresponding public key
     bool signatureOk { pubKey->verify(message, signature) };
 
+    assert(newPubKey->getContent() == newKey->getPubKey()->getContent());
+
     if(signatureOk)
-        std::cerr << "Signature verified ok" << std::endl;
+      std::cerr << "Signature verified ok" << std::endl;
+    }
+
 }
+//int main()
+//{
+//    /* Sign and verify a message */
+//
+//    // Get an arbitrary message for signing
+//    // (The contents are unimportant for this example)
+//    std::vector<uint8_t> message(16);
+//
+//    // Get public and private keys
+//    KeyPtr key { std::make_unique<KeyECSecp256k1<>>() };
+//    PubKeyPtr pubKey { key->getPubKey() };
+//
+//    // Sign the message using our private key
+//    std::vector<uint8_t> signature { key->sign(message) };
+//
+//    // Verify the signed message using the corresponding public key
+//    bool signatureOk { pubKey->verify(message, signature) };
+//
+//    if(signatureOk)
+//        std::cerr << "Signature verified ok" << std::endl;
+//}
 
